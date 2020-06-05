75b7a068d0e1d8be10e6d9e4194d3c90c3c3fcad
Ivan Lei <ilei0893@gmail.com>
2020-06-05 14:48:58 -0400

    Fixed URL, receiving and displaying api data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-import GifCard from "./Components/GifCard"
-import './App.css';
+import React, { Component } from "react";
+import GifCard from "./Components/GifCard";
+import "./App.css";
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <GifCard />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <GifCard />
+    </div>
+  );
+};
 
 export default App;
